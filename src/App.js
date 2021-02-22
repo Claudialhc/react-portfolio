0f747a2becd91264be60d6ceb864ba1bf8d67149
Claudia Hernandez <claudiahdez13@gmail.com>
2021-02-22 08:38:21 -0800

    added routing to app js file and imported fromt the respective pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
+import ContactPage from './components/ContactPage';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AboutMe />
+      <Portfolio />
+      <Resume />
+      <Footer />
+      <ContactPage />
     </div>
   );
 }
